99a7ed03c86566a50af2949c9f0b3c5d694eadad
tayele-alt <tayele@caldwell.edu>
2026-04-29 20:32:30 +0000

    Apr 29, 2026, 4:32 PM

--- src/main.py
+++ src/main.py
@@ -6,3 +6,18 @@ APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def get_data_path(filename):
     return os.path.join(APP_PATH, "data", filename)
+
+@st.cache_data
+def load_drivers():
+    df = pd.read.csv(get_data_path("drivers.csv"))
+    return df
+
+@st.cache.data
+def load_results():
+    df = pd.read_cvs(get_data_path("results.cvs"))
+    return df
+
+@st.cache.data
+def load_constructor_standing():
+    df = pd.read_csv(get_data_path("constructor_standings.cvs"))
+    return df
