dc76aa0d79750c92af81de85529c309b3de34f81
tayele-alt <tayele@caldwell.edu>
2026-05-04 14:07:59 +0000

    May 4, 2026, 10:07 AM

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ if mode == "Drivers":
 
     if selected:
         driver_standings = pd.read_csv(get_data_path("driver_standings.csv"))
-        st
+        st.subheader
 
 else:
     st.subheader("Constructors Data")
