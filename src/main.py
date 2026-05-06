60c0b4b18a0c0eab76b080a3b8614680a6779470
tayele-alt <tayele@caldwell.edu>
2026-05-06 02:19:38 +0000

    May 5, 2026, 10:19 PM

--- src/main.py
+++ src/main.py
@@ -57,7 +57,7 @@ if selected:
         wins = len(results[(results["driverId"] == driver_Id) & (results["position"] == "1")])
         races_entered = len(results[results["driverId"] == driver_Id])
 
-        last_races = races.groupby("year")
+        last_races = races.groupby("year")["racedId"]
         champs = races[races["raceId"].isin(
             driver_standings[
                 (driver_standings["driverId"] == driver_Id) & 
