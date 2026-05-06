306707fbdef694e33fe471bf1be5e7ded685f39e
tayele-alt <tayele@caldwell.edu>
2026-05-06 22:29:48 +0000

    May 6, 2026, 6:29 PM

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ else:
             constructor = constructors[constructors["name"] == team]
             constructor_id = constructor["constructorId"].values[0]
 
-            team_wins = len(results[(results["constructorId"] == constructor_id) & ()])
+            team_wins = len(results[(results["constructorId"] == constructor_id) & (results["posotion"])])
 
             team_stats.append({
                 "Teams": team,
