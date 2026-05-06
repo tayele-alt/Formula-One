c345576d4c2eabc0938d2ab20ff9baf890c97399
tayele-alt <tayele@caldwell.edu>
2026-05-06 18:36:07 +0000

    May 6, 2026, 2:36 PM

--- src/main.py
+++ src/main.py
@@ -52,7 +52,7 @@ else:
 
         team_stats = []
         for team in selected_teams:
-            
+            constructor = constructors[]
 
 if mode == "Drivers" and selected:
     driver_standings = pd.read_csv(get_data_path("driver_standings.csv"))
