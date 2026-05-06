b0067ac2e42d96ee8d56f04892c782075056035b
tayele-alt <tayele@caldwell.edu>
2026-05-06 02:30:36 +0000

    May 5, 2026, 10:30 PM

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ else:
 
     constructors = pd.read_csv(get_data_path("constructors.csv"))
     team_names = constructors["name"].tolist()
-    selected_team = st.multiselect()
+    selected_team = st.multiselect("Pick up to 4 teams:")
 
 if selected:
     driver_standings = pd.read_csv(get_data_path("driver_standings.csv"))
