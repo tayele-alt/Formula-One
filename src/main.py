190535ce050ca07b073a4b5af4fbeecaccaffd19
tayele-alt <tayele@caldwell.edu>
2026-05-06 18:37:24 +0000

    May 6, 2026, 2:37 PM

--- src/main.py
+++ src/main.py
@@ -54,6 +54,7 @@ else:
         for team in selected_teams:
             constructor = constructors[constructors["name"] == team]
             constructor_id = constructor["constructorId"].values[0]
+            
 
 if mode == "Drivers" and selected:
     driver_standings = pd.read_csv(get_data_path("driver_standings.csv"))
