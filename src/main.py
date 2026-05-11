9dd633d8b3779028950d969d569c0ca89fe4b463
tayele-alt <tayele@caldwell.edu>
2026-05-11 11:02:15 +0000

    May 11, 2026, 7:02 AM

--- src/main.py
+++ src/main.py
@@ -49,6 +49,7 @@ races = load_races()
 
 if mode == "Drivers":
     st.subheader("Select Drivers to Compare")
+    default = st
     driver_name = drivers["forename"] + " " + drivers["surname"]
     selected = st.multiselect("Pick up to 4 drivers:", driver_name, max_selections=4)
 
