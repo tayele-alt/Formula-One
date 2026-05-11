d8f87062eda00aff899734bc7eb29ee64e0d7c92
tayele-alt <tayele@caldwell.edu>
2026-05-11 03:10:07 +0000

    May 10, 2026, 11:10 PM

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ else:
             team_races = len(results[results["constructorId"] == constructor_id])
             team_wins = len(results[(results["constructorId"] == constructor_id) & (results["position"] == "1")])
             seasons = races[races["raceId"].isin(
-                results[results["constructorId"]]
+                results[results["constructorId"] == constructor_id]
             )]
 
             last_races = races.groupby("year")["raceId"].max()
