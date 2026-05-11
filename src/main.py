ede74a4aa25032322a60314fa6ea4f82e4cd101c
tayele-alt <tayele@caldwell.edu>
2026-05-11 12:55:50 +0000

    May 11, 2026, 8:55 AM

--- src/main.py
+++ src/main.py
@@ -91,7 +91,7 @@ else:
         st.dataframe(pd.DataFrame(team_stats))
 
         st.subheader("Wins Comparison")
-        team_chart = pd.DataFrame
+        team_chart = pd.DataFrame(team)
 
 st.sidebar.subheader("Load Comparison")
 saved = load_saved_comparisons()
