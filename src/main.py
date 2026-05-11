e48b6c8c009fc03cf72912474f2ae56782fa7acd
tayele-alt <tayele@caldwell.edu>
2026-05-11 10:25:47 +0000

    May 11, 2026, 6:25 AM

--- src/main.py
+++ src/main.py
@@ -117,4 +117,4 @@ if mode == "Drivers" and selected:
 
         driver_races = results[results["driverId"] == driver_Id]
         merged = driver_races.merge(races[["racedId", "year" ]], on="raceId")
-        season_wins = merged[merged["position"] == ]
+        season_wins = merged[merged["position"] == "1"].group
