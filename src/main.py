a68875508aecc44006fa3b74d84a02862ce07e2f
tayele-alt <tayele@caldwell.edu>
2026-05-11 10:27:13 +0000

    May 11, 2026, 6:27 AM

--- src/main.py
+++ src/main.py
@@ -120,4 +120,4 @@ if mode == "Drivers" and selected:
         season_wins = merged[merged["position"] == "1"].groupby("year").size()
         wins_per_season.append(season_wins.rename(name))
 
-    season_df = pd.contact
+    season_df = pd.contact(wins_per_season)
