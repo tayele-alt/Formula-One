61490fdfe6e190be3bfc515506b83ac97a78db7d
tayele-alt <tayele@caldwell.edu>
2026-05-11 10:44:41 +0000

    May 11, 2026, 6:44 AM

--- src/main.py
+++ src/main.py
@@ -89,7 +89,7 @@ else:
             })
         st.dataframe(pd.DataFrame(team_stats))
 
-    st.sidebar
+    st.sidebar.sunheader()
 
 if mode == "Drivers" and selected:
     driver_standings = pd.read_csv(get_data_path("driver_standings.csv"))
